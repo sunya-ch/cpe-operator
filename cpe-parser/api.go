da237a4ff5157d33febd8427f0e09deec6194d13
Sunyanan Choochotkaew <sunyanan.choochotkaew1@ibm.com>
2023-06-20 14:22:50 +0900

    add fmwork parser
    Signed-off-by: Sunyanan Choochotkaew <sunyanan.choochotkaew1@ibm.com>

--- cpe-parser/api.go
+++ cpe-parser/api.go
@@ -32,6 +32,7 @@ var gromacsParser parser.Parser = parser.NewGromacsParser()
 var linpackParser parser.Parser = parser.NewLinpackParser()
 var timeParser parser.Parser = parser.NewTimeParser()
 var stressParser parser.Parser = parser.NewStressParser()
+var fmworkParser parser.Parser = parser.NewFMWorkParser()
 
 var parserMap map[string]parser.Parser = map[string]parser.Parser{
 	"codait":   codaitParser,
@@ -47,6 +48,7 @@ var parserMap map[string]parser.Parser = map[string]parser.Parser{
 	"linpack":  linpackParser,
 	"time":     timeParser,
 	"stress":   stressParser,
+	"fmwork":   fmworkParser,
 }
 
 /////////////////////////////////////////////
